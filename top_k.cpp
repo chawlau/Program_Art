7aa34a0e12d9205b7c7292ca6e56b932d5fa1aac
liuchao <xiaonongyue@gmail.com>
2015-08-23 14:06:10 +0800

    top_k

--- top_k.cpp
+++ top_k.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdlib>
 
 void swap(int & left, int & right)
 {
@@ -16,6 +17,12 @@ void scan(int * array, int length)
     std::cout << array[index] << " ";
   }
   std::cout << std::endl;
+}
+
+int my_rand(int low, int high)
+{
+  int size = high - low + 1;
+  return low + rand()%size;
 }
 
 //以第一个元素对数组进行一次划分
@@ -48,13 +55,41 @@ int partion_rear(int * array, int left, int right)
   return pos;
 }
 
+//随机抽取一个数对数组进行切分
+int random_partion(int array[], int left, int right)
+{
+  int index = my_rand(left, right);
+  swap(array[right], array[index]);
+  //return partion_rear(array, left, right);
+  return partion(array, left, right);
+}
+
+int random_select(int array[], int left, int right, int k)
+{
+  if(k < 1 || k > (right - left + 1))
+  {
+    return -1;
+  }
+  int pos = random_partion(array, left, right);
+  int m = pos - left + 1;
+  if (m == k)
+  {
+    return array[pos];
+  }
+  else if (m > k)
+  {
+    return random_select(array, left, pos - 1, k);
+  }
+  else 
+  {
+    return random_select(array, pos + 1, right, k - m);
+  }
+}
 int main(int argc,char** argv)
 {
   int array[8] = {6 , 7 , 2, 4 , 1 , 3, 9, 5};
   scan(array, 8);
-  partion(array, 0, sizeof(array)/sizeof(int)-1);
-  //partion_rear(array, 0, 7) ;
-  scan(array, 8);
+  std::cout << random_select(array, 0, 7, 6) <<std::endl;
 }
 
 
